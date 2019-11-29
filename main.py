c19eab858a48ee9d445fdc646520e57368c8b22b
yusuf canbaz <yusufsaidcanbaz@gmail.com>
2019-11-29 23:15:16 +0300

    resize img, start of feature extraction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from skimage.segmentation import slic
 from skimage.segmentation import mark_boundaries
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 import numpy as np
 
 
@@ -18,7 +19,7 @@ def get_adjacency_matrix(segments: np.array):
             if seg1 != seg2:
                 r[seg1][seg2] = 1
                 r[seg2][seg1] = 1
-    
+
     # vertically iterate over pixels
     for j in range(y):
         for i in range(j + 1, x - 1):
@@ -27,15 +28,22 @@ def get_adjacency_matrix(segments: np.array):
             if seg1 != seg2:
                 r[seg1][seg2] = 1
                 r[seg2][seg1] = 1
-    
+
     return r
+
+
+def extract_features():
+    pass
+
 
 fname = 'img\\SEToriginalWristImages\\SET1\\0001_01_01_02_863_695_288_408_L.jpg'
 num_superpixel = 200
+img_height = 200
 compactness = 8
 
 img = plt.imread(fname)
+img = resize(img, (img_height, img.shape[1]))
 segments = slic(img, n_segments=num_superpixel, compactness=compactness)
 adj_matrix = get_adjacency_matrix(segments)
 
-print (adj_matrix)
+print(adj_matrix)
