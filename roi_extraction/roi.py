130a5ab7fef4a8df6b886ce6d273923fee98a749
yusuf canbaz <yusufsaidcanbaz@gmail.com>
2019-12-15 19:28:42 +0300

    start build graph

--- roi_extraction/roi.py
+++ roi_extraction/roi.py
@@ -5,9 +5,9 @@ from skimage.transform import rescale
 
 DB_PATH = 'D:\\\\yusuf\\\\cs 579\\\\project\\\\NTU-Wrist-Image-Database-v1'
 
-# to show vertical wrinkles apply vertical sobel filter
-def get_img_weight(img):
 
+def get_img_weight(img):
+    """ to show vertical wrinkles apply vertical sobel filter """
     Gx = np.array([])
     # for each image channel
     for i in range(img.shape[2]):
@@ -24,31 +24,45 @@ def get_img_weight(img):
     Gx = rescale(Gx, 0.2, multichannel=False)
     return Gx
 
-# return row indices of up and down lines
+
 def get_up_down_boundaries(mask: np.array):
+    """ return row indices of up and down lines, corresponds to maskDetermineEdges in paper code """
     _, n = mask.shape
-    
+
     r = np.ones((2, 2 * n), dtype=int)
     idx_arr = np.array(range(n)).T
     r[0] = np.concatenate((idx_arr, idx_arr))
-    
+
     for i in range(n):
         idxs = np.nonzero(mask[:, i])[0]
         r[1][i] = np.min(idxs)
         r[1][i + n] = np.max(idxs)
     return r
 
+
+def build_graph(img, mask, connectivity, diag_dist, power_param)
+    """ corresponds to 'function G = maskBuildGraph(I,mask,conn, diagParamD,powerParam)'
+    img should be weighted image """
+    if connectivity != 4 or connectivity != 8:
+        print ('connectivity must be 4 or 8, it is being set to 4')
+        connectivity = 4
+    
 def test_get_img_weight():
-    img = plt.imread('D:\\yusuf\\cs 579\\project\\NTU-Wrist-Image-Database-v1\\SETsegmentedWristImages\\SET1\\img\\0001_01_01_01_809_866_318_423_binTree_L.jpg')
+    img = plt.imread(
+        'D:\\yusuf\\cs 579\\project\\NTU-Wrist-Image-Database-v1\\SETsegmentedWristImages\\SET1\\img\\0001_01_01_01_809_866_318_423_binTree_L.jpg')
     get_img_weight(img)
 
+
 def test_get_up_down_boundaries():
-    mask = plt.imread('D:\\yusuf\\cs 579\\project\\NTU-Wrist-Image-Database-v1\\SETsegmentedWristImages\\SET1\\mask\\mask0001_01_01_01_809_866_318_423_binTree_L.jpg')
-    mask = rescale(mask, 0.2, multichannel=False, preserve_range=True, anti_aliasing=False)
+    mask = plt.imread(
+        'D:\\yusuf\\cs 579\\project\\NTU-Wrist-Image-Database-v1\\SETsegmentedWristImages\\SET1\\mask\\mask0001_01_01_01_809_866_318_423_binTree_L.jpg')
+    mask = rescale(mask, 0.2, multichannel=False,
+                   preserve_range=True, anti_aliasing=False)
     bounds = get_up_down_boundaries(mask)
     mask[bounds[1], bounds[0]] = 150
     plt.imshow(mask)
     plt.show()
+
 
 # test_get_img_weight()
 test_get_up_down_boundaries()
